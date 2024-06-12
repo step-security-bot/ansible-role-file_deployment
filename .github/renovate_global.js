c4685722cae4364c98fef1b0a6f32c24b908ba01
Steffen Scheib <steffen@scheib.me>
2024-06-12 21:31:02 +0200

    ci: Trying to fix Renovate secrets

--- .github/renovate_global.js
+++ .github/renovate_global.js
@@ -5,20 +5,20 @@ module.exports = {
         'galaxy-collection',
       ],
       registryUrls: [
-        'https://console.redhat.com/api/automation-hub/v3/plugin/ansible/content/published/',
-        'https://console.redhat.com/api/automation-hub/v3/plugin/ansible/content/validated/',
+        'https://console.redhat.com/api/automation-hub/content/published/',
+        'https://console.redhat.com/api/automation-hub/content/validated/',
         'https://galaxy.ansible.com',
       ]
     },
   ],
   hostRules: [
     {
-      matchHost: 'https://console.redhat.com/api/automation-hub/v3/plugin/ansible/content/published/',
+      matchHost: 'https://console.redhat.com/api/automation-hub/content/published/',
       username: process.env.RENOVATE_AUTOMATION_HUB_USERNAME,
       password: process.env.RENOVATE_AUTOMATION_HUB_PASSWORD,
     },
     {
-      matchHost: 'https://console.redhat.com/api/automation-hub/v3/plugin/ansible/content/validated/',
+      matchHost: 'https://console.redhat.com/api/automation-hub/content/validated/',
       username: process.env.RENOVATE_AUTOMATION_HUB_USERNAME,
       password: process.env.RENOVATE_AUTOMATION_HUB_PASSWORD,
     },
