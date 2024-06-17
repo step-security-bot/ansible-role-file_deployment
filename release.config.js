d02c1cef390147fce93cb66fb4fc2e4dde5ecc29
Steffen Scheib <steffen@scheib.me>
2024-06-17 13:59:04 +0200

    ci: Rewriting config

--- release.config.js
+++ release.config.js
@@ -1,30 +1,21 @@
 module.exports = {
-  preset: 'conventionalcommits',
-  // Conventional Changelog config specifications can be found here:
-  // -> https://github.com/conventional-changelog/conventional-changelog-config-spec/tree/master/versions
-  presetConfig: {
-    types: [
-      {type: 'feat', section: '🌟 Features'},
-      {type: 'fix', section: '🐛 Bug Fixes'},
-      {type: 'perf', section: '⚡ Performances Improvements'},
-      {type: 'revert', section: '🔄 Revert'},
-      {type: 'refactor', section: '🛠️ Refactoring'},
-      {type: 'build', section: '🏗️ Build System'},
-      {type: 'test', section: '✅ Tests'},
-      {type: 'ci', section: '📦 Continuous Integration'},
-      {type: 'docs', section: '📖 Documentation'},
-      {type: 'chore', section: '🧹 House Keeping'}
-    ]
-  },
   branches: [
-    'main',
-    {name: 'beta', prerelease: true},
-    {name: 'alpha', prerelease: true}
+    {
+      name: 'main',
+      prerelease: false,
+    },
+    {
+      name: 'beta',
+      prerelease: true,
+    },
+    {
+      name: 'alpha',
+      prerelease: true,
+    }
   ],
   plugins: [
     '@semantic-release/commit-analyzer',
     '@semantic-release/release-notes-generator',
-    '@semantic-release/changelog',
     '@semantic-release/github',
   ]
 };
